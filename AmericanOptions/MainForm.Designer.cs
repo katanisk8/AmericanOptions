fac56d8d7ed1651a827af669868c425985bf1676
Michał Makowej <michal@makowej.pl>
2018-04-16 10:02:04 +0200

    Organization

--- AmericanOptions/MainForm.Designer.cs
+++ AmericanOptions/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AmericanOptions
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+         this.SuspendLayout();
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(684, 361);
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.MaximizeBox = false;
+         this.Name = "MainForm";
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         this.Text = "American Options";
+         this.Load += new System.EventHandler(this.MainForm_Load);
+         this.ResumeLayout(false);
+
         }
 
         #endregion
--- AmericanOptions/Program.cs
+++ AmericanOptions/Program.cs
@@ -16,7 +16,7 @@ namespace AmericanOptions
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
