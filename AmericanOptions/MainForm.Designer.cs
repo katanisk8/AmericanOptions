62ad13033040e8cfb26e0bfcc0de8d9fe95002bc
Michał Makowej <michal@makowej.pl>
2018-04-16 14:30:27 +0200

    CalculateButton

--- AmericanOptions/MainForm.Designer.cs
+++ AmericanOptions/MainForm.Designer.cs
@@ -29,13 +29,48 @@
         private void InitializeComponent()
         {
          System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+         this.CalculateButton = new System.Windows.Forms.Button();
+         this.groupBox1 = new System.Windows.Forms.GroupBox();
+         this.groupBox2 = new System.Windows.Forms.GroupBox();
          this.SuspendLayout();
+         // 
+         // CalculateButton
+         // 
+         this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+         this.CalculateButton.Location = new System.Drawing.Point(572, 319);
+         this.CalculateButton.Name = "CalculateButton";
+         this.CalculateButton.Size = new System.Drawing.Size(100, 30);
+         this.CalculateButton.TabIndex = 1;
+         this.CalculateButton.Text = "Calculate";
+         this.CalculateButton.UseVisualStyleBackColor = true;
+         this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+         // 
+         // groupBox1
+         // 
+         this.groupBox1.Location = new System.Drawing.Point(12, 12);
+         this.groupBox1.Name = "groupBox1";
+         this.groupBox1.Size = new System.Drawing.Size(200, 100);
+         this.groupBox1.TabIndex = 2;
+         this.groupBox1.TabStop = false;
+         this.groupBox1.Text = "groupBox1";
+         // 
+         // groupBox2
+         // 
+         this.groupBox2.Location = new System.Drawing.Point(12, 118);
+         this.groupBox2.Name = "groupBox2";
+         this.groupBox2.Size = new System.Drawing.Size(200, 100);
+         this.groupBox2.TabIndex = 3;
+         this.groupBox2.TabStop = false;
+         this.groupBox2.Text = "groupBox2";
          // 
          // MainForm
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(684, 361);
+         this.Controls.Add(this.groupBox2);
+         this.Controls.Add(this.groupBox1);
+         this.Controls.Add(this.CalculateButton);
          this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
          this.MaximizeBox = false;
          this.Name = "MainForm";
@@ -46,7 +81,11 @@
 
         }
 
-        #endregion
-    }
+      #endregion
+
+      private System.Windows.Forms.Button CalculateButton;
+      private System.Windows.Forms.GroupBox groupBox1;
+      private System.Windows.Forms.GroupBox groupBox2;
+   }
 }
 
--- AmericanOptions/MainForm.cs
+++ AmericanOptions/MainForm.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AmericanOptions
 {
-    public partial class MainForm : Form
+   public partial class MainForm : Form
     {
         public MainForm()
         {
@@ -18,6 +11,11 @@ namespace AmericanOptions
         }
 
       private void MainForm_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void CalculateButton_Click(object sender, EventArgs e)
       {
 
       }
