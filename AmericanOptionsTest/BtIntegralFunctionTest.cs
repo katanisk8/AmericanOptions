93c56cb86cf183bb4a3ea3ebb8b38e4bd235e410
Michał Makowej <michal@makowej.pl>
2018-05-21 21:22:52 +0200

    BtIntegralFunctionTest

---
+++ AmericanOptionsTest/BtIntegralFunctionTest.cs
@@ -0,0 +1,27 @@
+﻿using AmericanOptions.OptimalExerciseBoundary;
+using Moq;
+using Xunit;
+
+namespace AmericanOptionsTest
+{
+    public class BtIntegralFunctionTest
+    {
+        int n = 4;
+        double T = 1;
+        double r = 0.05;
+        double sigma = 0.2;
+        double t = 1;
+        double d2 = -0.81945721667909832;
+
+
+        [Fact]
+        public void CalculateTest()
+        {
+            Mock<BtIntegralFunction> integral = new Mock<BtIntegralFunction>();
+
+            double integralValue = integral.Object.Calculate(n, T, r, sigma, t, d2);
+
+            Assert.Equal(0.33245806127914307, integralValue);
+        }
+    }
+}
