f458109a1061209ba82568a6bd6d48a408dd150e
Michał Makowej <michal@makowej.pl>
2018-11-28 11:05:14 +0100

    Calculations Tests

--- AmericanOptionsTest/BtIntegralFunctionTest.cs
+++ AmericanOptionsTest/BtIntegralFunctionTest.cs
@@ -1,26 +1,26 @@
-﻿using AmericanOptions.OptimalExerciseBoundary;
+﻿using AmericanOptions.Model;
+using AmericanOptions.OptimalExerciseBoundary;
 using Xunit;
 
 namespace AmericanOptionsTest
 {
-    public class BtIntegralFunctionTest
-    {
+   public class BtIntegralFunctionTest
+   {
       private int n = 4;
       private double T = 1;
       private double r = 0.05;
       private double sigma = 0.2;
       private double t = 1;
-      private double d2 = -0.81945721667909832;
+      private IntegralPoint d2 = new IntegralPoint { Result = new Result { Value = -0.81945721667909832 } };
 
+      [Fact]
+      public async void CalculateTest()
+      {
+         BtIntegralFunction integral = new BtIntegralFunction();
 
-        [Fact]
-        public void CalculateTest()
-        {
-            BtIntegralFunction integral = new BtIntegralFunction();
+         var integralValue = await integral.CalculateAsync(n, T, r, sigma, t, d2);
 
-            double integralValue = integral.Calculate(n, T, r, sigma, t, d2);
-
-            Assert.Equal(0.33245806127914307, integralValue);
-        }
-    }
+         Assert.Equal(0.33245806127914307, integralValue.Result.Value);
+      }
+   }
 }
--- AmericanOptionsTest/IntegralPointsUnitTest.cs
+++ AmericanOptionsTest/IntegralPointsUnitTest.cs
@@ -3,33 +3,33 @@ using AmericanOptions.Helpers;
 
 namespace AmericanOptionsTest
 {
-    public class IntegralPointsUnitTest
-    {
+   public class IntegralPointsUnitTest
+   {
       private double S = 45;
       private double B = 45;
       private double r = 0.05;
       private double sigma = 0.2;
       private double t = 1;
 
-        [Fact]
-        public void CalculateIntegralPointD1Test()
-        {
-            IntegralPoints integralPoints = new IntegralPoints();
+      [Fact]
+      public void CalculateIntegralPointD1Test()
+      {
+         IntegralPoints integralPoints = new IntegralPoints();
 
-            double d1 = integralPoints.CalculateIntegralPointD1(S, B, r, sigma, t);
+         var d1 = integralPoints.CalculateIntegralPointD1(S, B, r, sigma, t);
 
-            Assert.Equal(0.35000000000000003, d1);
-        }
+         Assert.Equal(0.35000000000000003, d1.Result.Value);
+      }
 
-        [Fact]
-        public void CalculateIntegralPointD2Test()
-        {
-            IntegralPoints integralPoints = new IntegralPoints();
+      [Fact]
+      public void CalculateIntegralPointD2Test()
+      {
+         IntegralPoints integralPoints = new IntegralPoints();
 
-            double d1 = integralPoints.CalculateIntegralPointD1(S, B, r, sigma, t);
-            double d2 = integralPoints.CalculateIntegralPointD2(d1, sigma, t);
+         var d1 = integralPoints.CalculateIntegralPointD1(S, B, r, sigma, t);
+         var d2 = integralPoints.CalculateIntegralPointD2(d1, sigma, t);
 
-            Assert.Equal(0.15000000000000002, d2);
-        }
-    }
+         Assert.Equal(0.15000000000000002, d2.Result.Value);
+      }
+   }
 }
